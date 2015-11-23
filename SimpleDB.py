bfeee8d9513e88cd97447b3aebcf1828abe158ba
Justin Knichel <jtk21351@gmail.com>
2015-11-22 21:19:28 -0500

    Add some more comments for documentation

--- SimpleDB.py
+++ SimpleDB.py
@@ -12,27 +12,36 @@ class SimpleDB(object):
 
     def set(self, var, val):
         """Set key var to value val in current transaction"""
+        # if no open transactions, write directly to db
         if len(self.__transactions) == 0:
             self.__db[var] = val
+        # if open transaction(s) write to transaction log
         else:
             self.__transactions[-1][var] = val
 
     def get(self, var):
         """Get value of var in DB"""
+        # if no transactions, check db for var
         if len(self.__transactions) == 0:
             return self.__db.get(var, False)
 
+        # generate current state of working database to scan for var
         working_db = self.build_working_db()
 
+        # if value is in working_db, return it
         if var in working_db:
             return working_db[var]
+        # otherwise return False to print NULL
         else:
             return False
 
     def unset(self, var):
         """If value exists, set it to None for this transaction"""
+        # if no open transactions, pop directly from db
         if len(self.__transactions) == 0:
             self.__db.pop(var, None)
+        # otherwise, set value to None in current transaction to indicate
+        # it should be deleted on next commit
         else:
             self.__transactions[-1][var] = None
 
@@ -41,6 +50,7 @@ class SimpleDB(object):
         working_db = self.build_working_db()
         num_equal_to = 0
 
+        # scan working database for the values, and count them
         for var in working_db:
             if working_db[var] == val:
                 num_equal_to += 1
@@ -54,6 +64,7 @@ class SimpleDB(object):
     def rollback(self):
         """Removes and ignores changes from most recent transaction"""
         if len(self.__transactions) == 0:
+            # if no open transaction, raise exception defined below to indicate
             raise NoTransaction('NO TRANSACTION')
 
         del self.__transactions[-1]
@@ -63,6 +74,7 @@ class SimpleDB(object):
     def commit(self):
         """Commits changes from all transactions to self.__db"""
         if len(self.__transactions) == 0:
+            # if no open transaction, raise exception defined below to indicate
             raise NoTransaction('NO TRANSACTION')
 
         self.__db = self.build_working_db()
@@ -71,7 +83,10 @@ class SimpleDB(object):
         return True
 
     def build_working_db(self):
-        """Builds a version of what db would look like after a commit"""
+        """Builds a version of what db would look like after a commit by
+        collapsing all open transactions, keeping the most recent values for
+        every variable, and writing, deleting, or overwriting to the original
+        db as needed."""
         if len(self.__transactions) == 0:
             return self.__db
 
@@ -89,6 +104,7 @@ class SimpleDB(object):
         return working_db
 
 class NoTransaction(Exception):
+    """An exception for attempt to close a transaction when there isn't one."""
     def __init__(self, value):
         self.value = value
     def __str__(self):
@@ -96,6 +112,7 @@ class NoTransaction(Exception):
 
 
 def process_command(command, db):
+    """Take in command string, and act accordingly"""
     command = command.split(' ')
     if command[0] == 'END':
         return
@@ -152,6 +169,7 @@ def process_command(command, db):
         print 'Not a valid command, please try again'
 
 def main():
+    """A main function to start the script and call process_command"""
     db = SimpleDB()
     command = ''
     while command != 'END':
